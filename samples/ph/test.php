02807c1179a3cf4dac32892050466a94a4c7dc57
Joel2B <jbuenouas@gmail.com>
2022-05-29 19:31:04 -0600

    chore: remove unused code

--- samples/ph/test.php
+++ samples/ph/test.php
@@ -13,8 +13,6 @@ if (!preg_match('/document\.cookie="RNKEY="\+n\+/', $content)) {
     $content = file_get_contents('sample.html');
 }
 
-preg_match('/function(?:[^}]*+})++/', $content, $matches);
-
 include '../../inc/ph/second-method.php';
 
 header('Content-Type: text/html');
